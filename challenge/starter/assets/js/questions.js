a9d664e6f7b2e012f18e310330326bebcaa2e83e
Hamdi <hamdishh2003@gmail.com>
2023-12-12 02:21:34 +0000

    Add changes to my repo

---
+++ challenge/starter/assets/js/questions.js
@@ -0,0 +1,21 @@
+// 1. create an array of the questions that will be used in the quiz
+var myQuestions = [
+    {
+      question: 'Commonly used data types DO NOT include?',
+      options: [
+        { text: '1. Strings', correct: false },
+        { text: '2. Booleans', correct: false },
+        { text: '3. alerts', correct: true },
+        { text: '4. numbers', correct: false },
+      ]
+    },
+    {
+     question: 'The condition in an if/else statement is enclosed within___.',
+     options: [
+       { text: '1. quotes', correct: false },
+       { text: '2. curly brackets', correct: false },
+       { text: '3. parenthesis', correct: true },
+       { text: '4. square brackets', correct: false},
+     ]
+    }
+];
